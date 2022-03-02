99d75710555bb73c1045f08aa710fd6c640089da
carbonnm <marie.carbonnelle@student.unamur.be>
2022-03-02 22:41:31 +0100

    hardcodage d'une partie

--- src/web/application/models.py
+++ src/web/application/models.py
@@ -227,6 +227,122 @@ db.create_all()
 utilisateurs = Account(username = "carbonnm", lastName = "Carbonnelle", firstName = "Marie", email = "marie.carbonnelle@student.unamur.be", password = generate_password_hash("Dzhari50"))
 db.session.add(utilisateurs)
 
+
+#Hardcode of a very simple game to test (for convenience too)
+#Simple game part
+basicGame = Game(gameName = "Physique simple", nbThemes = 3, nbQuestions = 3, URL = "physique", creator = "carbonnm")
+db.session.add(basicGame)
+
+#Themes part
+theme1 = Theme(themeName = "Lois de Newton", associatedGame = "Physique simple")
+theme2 = Theme(themeName = "Vitesse", associatedGame = "Physique simple")
+theme3 = Theme(themeName = "Ondes", associatedGame = "Physique simple")
+
+db.session.add(theme1)
+db.session.add(theme2)
+db.session.add(theme3)
+
+#Questions part
+question1 = Question(enonce = "Si je pèse 70 kg et que l'on considère que la gravité vaut 10m/s2, Que vaut la force qui s'applique sur moi en ce moment ?", 
+                    reponseA = "70 N", 
+                    reponseB = "7000 N",
+                    reponseC = "700 N", 
+                    reponseD = "7 N",
+                    bonneReponse = "700 N",
+                    difficultyLevel = 1,
+                    nbPoints = 1,
+                    associatedTheme = "Lois de Newton")
+
+question2 = Question(enonce = "Quelle est la deuxième loi de Newton", 
+                    reponseA = "Tout objet non soumis à des forces conserve son état de repos ou de mouvement rectiligne et uniforme", 
+                    reponseB = "F = m a",
+                    reponseC = "Action et réaction: si un objet exerce une force F sur un second objet, celui-ci exerce à son tour une force -F sur le premier.", 
+                    reponseD = "Les forces de frottements.",
+                    bonneReponse = "F = m a",
+                    difficultyLevel = 2,
+                    nbPoints = 2,
+                    associatedTheme = "Lois de Newton")
+
+question3 = Question(enonce = "Quelle loi de Newton permet d'expliquer pourquoi on peut sentir les choses que l'on touche ?", 
+                    reponseA = "Première loi de Newton ou principe de l'inertie.", 
+                    reponseB = "Deuxième loi de Newton ou principe fondamental de la dynamique.",
+                    reponseC = "Troisième loi de Newton ou principe de l'action et de la réaction.", 
+                    reponseD = "Aucune des réponses ci-dessus.",
+                    bonneReponse = "Troisième loi de Newton ou principe de l'action et de la réaction.",
+                    difficultyLevel = 3,
+                    nbPoints = 3,
+                    associatedTheme = "Lois de Newton")
+
+question4 = Question(enonce = "Quelle est lunité réglementaire de la vitesse (selon le SI) ?", 
+                    reponseA = "km/h", 
+                    reponseB = "(m/s)/2",
+                    reponseC = "m/s", 
+                    reponseD = "km/s",
+                    bonneReponse = "m/s",
+                    difficultyLevel = 1,
+                    nbPoints = 1,
+                    associatedTheme = "Vitesse")
+
+question5 = Question(enonce = "Quelle est la formule de la vitesse ?", 
+                    reponseA = "Vitesse = Distance/Temps", 
+                    reponseB = "Vitesse = masse x accélération",
+                    reponseC = "Vitesse = Emc2", 
+                    reponseD = "Vitesse = Energie interne + Energie mécanique",
+                    bonneReponse = "Vitesse = Distance/Temps",
+                    difficultyLevel = 2,
+                    nbPoints = 2,
+                    associatedTheme = "Vitesse")
+
+question6 = Question(enonce = "Ophélie a parcouru 62,5 km à la vitesse de 40 km/h. Quelle est la durée du trajet ? ", 
+                    reponseA = "1 heure 46 minutes", 
+                    reponseB = "33 minutes 45 secondes",
+                    reponseC = "46 minutes et 1 seconde", 
+                    reponseD = "45 minutes et 33 secondes",
+                    bonneReponse = "33 minutes 45 secondes",
+                    difficultyLevel = 3,
+                    nbPoints = 3,
+                    associatedTheme = "Vitesse")
+
+question7 = Question(enonce = "Quelles sont les ondes produites lorsqu'on téléphone ?", 
+                    reponseA = "Des ondes gravitationnelles", 
+                    reponseB = "Des ondes sonores",
+                    reponseC = "Des ondes sysmiques", 
+                    reponseD = "Des ondes radios",
+                    bonneReponse = "Des ondes radios",
+                    difficultyLevel = 1,
+                    nbPoints = 1,
+                    associatedTheme = "Ondes")
+
+question8 = Question(enonce = "Lequel utilise les longueurs d'ondes les plus courtes ?", 
+                    reponseA = "L'infrarouge", 
+                    reponseB = "La lumière visible",
+                    reponseC = "L'ultraviolet", 
+                    reponseD = "La lumière bleue",
+                    bonneReponse = "L'ultraviolet",
+                    difficultyLevel = 2,
+                    nbPoints = 2,
+                    associatedTheme = "Ondes")
+
+question9 = Question(enonce = "Quel type d'ondes émet la chauve-souris pour se déplacer ?", 
+                    reponseA = "Des ultrasons", 
+                    reponseB = "De l'infrarouge",
+                    reponseC = "De l'électromagnétique", 
+                    reponseD = "De l'ultraviolet",
+                    bonneReponse = "Des ultrasons",
+                    difficultyLevel = 3,
+                    nbPoints = 3,
+                    associatedTheme = "Ondes")
+
+db.session.add(question1)
+db.session.add(question2)
+db.session.add(question3)
+db.session.add(question4)
+db.session.add(question5)
+db.session.add(question6)
+db.session.add(question7)
+db.session.add(question8)
+db.session.add(question9)
+
 db.session.commit()
 
 
